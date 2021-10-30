91f208f56fcf458c575cdccb9819c14811a973ed
fajrullah <fajrullahsejati_9_7@yahoo.com>
2021-10-30 22:46:37 +0700

    pg fixed

--- core/database/index.js
+++ core/database/index.js
@@ -6,8 +6,16 @@ const Sequelize = require('sequelize')
 const config = require('../config')
 const check = require('../config/check')
 const { initModels } = require('./models/init-models')
-const sequelize = new Sequelize(config.db_url)
+// const sequelize = new Sequelize('postgres://qydpxkqrqxurcu:5c603f72a3c25aa6ecbe42c5cd61606f3bb891148d5386b262e3d56de81b7951@ec2-3-217-234-137.compute-1.amazonaws.com:5432/d1a8ectr80o8pu')
 
+const sequelize = new Sequelize(config.db_url, {
+  dialect: 'postgres',
+  native: true,
+  ssl: true,
+  dialectOptions: {
+    ssl: true
+  }
+})
 
 sequelize.authenticate()
   .then(() => {
