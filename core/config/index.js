b1600942135d2d15e8cdfaa0a8b02a36420920fe
fajrullah <fajrullahsejati_9_7@yahoo.com>
2021-10-31 13:15:42 +0700

    db url

--- core/config/index.js
+++ core/config/index.js
@@ -13,5 +13,5 @@ module.exports = {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   dbport: process.env.DB_PORT,
-  db_url: 'postgres://qydpxkqrqxurcu:5c603f72a3c25aa6ecbe42c5cd61606f3bb891148d5386b262e3d56de81b7951@ec2-3-217-234-137.compute-1.amazonaws.com:5432/d1a8ectr80o8pu'
+  db_url: 'example'
 }
